e9df92bb030857d8aaea90408bd7ee1116007403
Aleksandr Semenov <semenlinux@gmail.com>
2020-11-25 14:49:10 +0300

    Add dynamic page generation. Automatic calculation of the age of the winery.

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
+import datetime
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+template = env.get_template('template.html')
+
+birthday_of_wine = 1920
+current_year = datetime.datetime.now().year
+age = current_year - birthday_of_wine
+
+rendered_page = template.render(
+    age = age,
+)
+
+with open('index.html', 'w', encoding="utf8") as file:
+    file.write(rendered_page)
 
 server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
 server.serve_forever()
