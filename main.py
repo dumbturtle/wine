784f9b28c1abc3d5143811acaf44d9e1020fab93
Aleksandr Semenov <semenlinux@gmail.com>
2020-11-25 16:42:37 +0300

    Add generate list of wine from xlsx

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import datetime
+import pandas
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
@@ -10,12 +11,18 @@ env = Environment(
 
 template = env.get_template('template.html')
 
+#Расчет возраста
 birthday_of_wine = 1920
 current_year = datetime.datetime.now().year
 age = current_year - birthday_of_wine
 
+#Перечень вина
+excel_data_df = pandas.read_excel('wine.xlsx', sheet_name='Лист1', usecols=['Название', 'Сорт', 'Цена', 'Картинка'])
+dict_of_wines = excel_data_df.to_dict(orient='records')
+
 rendered_page = template.render(
     age = age,
+    wines = dict_of_wines,
 )
 
 with open('index.html', 'w', encoding="utf8") as file:
