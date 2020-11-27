c6f3ad583fcf7c5e54ebfee49dbfd61fca0f0a84
Aleksandr Semenov <semenlinux@gmail.com>
2020-11-27 16:30:38 +0300

    Added the ability to specify the path to the file with the description of wines

--- main.py
+++ main.py
@@ -1,36 +1,48 @@
+import argparse
 import datetime
-import pandas
+import os
+import sys
 from collections import defaultdict
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
+import pandas
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
+parser = argparse.ArgumentParser(
+    description='Сайт по продаже вина'
+)
 env = Environment(
     loader=FileSystemLoader("."), autoescape=select_autoescape(["html", "xml"])
 )
 
 template = env.get_template("template.html")
 
-birthday_of_wine = 1920
+birthday_of_wine_shop = 1920
 current_year = datetime.datetime.now().year
-age = current_year - birthday_of_wine
+shop_age = current_year - birthday_of_wine_shop
 
-excel_data_df = pandas.read_excel(
-    "wine3.xlsx",
+parser.add_argument('filename', help='File name of wine description.', nargs='?', default='default_description.xlsx')
+args = parser.parse_args()
+filename = args.filename
+
+if not os.path.exists(filename):
+    sys.exit('File with descriptions is not found!')
+
+wine_descriptions_form_file = pandas.read_excel(
+    filename,
     sheet_name="Лист1",
     na_values=" ",
     keep_default_na=False,
     usecols=["Категория", "Название", "Сорт", "Цена", "Картинка", "Акция"],
 )
 
-dict_of_wines = defaultdict(list)
-for data in excel_data_df.to_dict("records"):
-    dict_of_wines[data["Категория"]].append(data)
-
+wine_descriptions = defaultdict(list)
+for description in wine_descriptions_form_file.to_dict("records"):
+    wine_descriptions[description["Категория"]].append(description)
 
 rendered_page = template.render(
-    age=age,
-    wines=dict_of_wines,
+    shop_age=shop_age,
+    wine_descriptions=wine_descriptions,
 )
 
 with open("index.html", "w", encoding="utf8") as file:
